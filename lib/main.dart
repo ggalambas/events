1fbfcb22ac9e3c57964bcf13df7a65758644e4e6
agalambas <agalambas@gmail.com>
2020-11-23 21:52:02 +0000

    provider fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:events/app/scroll_model.dart';
 import 'package:events/injection.dart';
 import 'package:events/router.gr.dart';
 import 'package:events/ui/components/base_widget.dart';
@@ -6,6 +7,7 @@ import 'package:events/ui/config/custom_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   configureInjection(Environment.prod);
@@ -30,19 +32,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return BaseWidget<CustomTheme>(
-      model: CustomTheme(),
-      builder: (_, theme, __) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false, //!
-          theme: theme.light,
-          darkTheme: theme.dark,
-          themeMode: theme.mode,
-          builder: ExtendedNavigator.builder<ARouter>(
-            router: ARouter(),
-          ),
-        );
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CustomTheme>(create: (_) => CustomTheme()),
+        ChangeNotifierProvider<ScrollModel>(create: (_) => ScrollModel()),
+      ],
+      child: Consumer<CustomTheme>(
+        builder: (_, theme, __) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false, //!
+            theme: theme.light,
+            darkTheme: theme.dark,
+            themeMode: theme.mode,
+            builder: ExtendedNavigator.builder<ARouter>(
+              router: ARouter(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,7 @@
-import 'package:events/ui/components/base_widget.dart';
 import 'package:events/ui/components/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:events/app/scroll_model.dart';
+import 'package:provider/provider.dart';
 
 List<String> concelhos = [
   'Mafra',
@@ -21,38 +21,34 @@ List<String> concelhos = [
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BaseWidget<ScrollModel>(
-      model: ScrollModel(),
-      builder: (_, scroll, __) {
-        return Scaffold(
-          drawer: Drawer(),
-          body: NotificationListener<ScrollEndNotification>(
-            onNotification: (_) {
-              scroll.snapFlexbar();
-              return false;
-            },
-            child: CustomScrollView(
-              controller: scroll.controller,
-              slivers: <Widget>[
-                CustomAppBar(
-                  title: 'Categoria', //!
-                ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (_, index) {
-                      return SizedBox(
-                        height: 100,
-                        child: Text(concelhos[index]),
-                      );
-                    },
-                    childCount: concelhos.length,
-                  ),
-                )
-              ],
+    final ScrollModel scroll = Provider.of<ScrollModel>(context, listen: false);
+    return Scaffold(
+      drawer: Drawer(),
+      body: NotificationListener<ScrollEndNotification>(
+        onNotification: (_) {
+          scroll.snapFlexbar();
+          return false;
+        },
+        child: CustomScrollView(
+          controller: scroll.controller,
+          slivers: <Widget>[
+            CustomAppBar(
+              title: 'Categoria', //!
             ),
-          ),
-        );
-      },
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (_, index) {
+                  return SizedBox(
+                    height: 100,
+                    child: Text(concelhos[index]),
+                  );
+                },
+                childCount: concelhos.length,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
